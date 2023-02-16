259bacb579f4c5b5e445690467019abe46299ce0
Andrew Piispanen <apiispanen1@babson.edu>
2023-02-16 09:26:34 -0500

    edits to dependencies

--- sound.py
+++ sound.py
@@ -8,16 +8,17 @@ from prompt import ai_response, save_conversation
 # 'ttkthemes'])
 
 
+import pyaudio
+import wave
+import os
+
+
 p = pyaudio.PyAudio()
 
 for i in range(p.get_device_count()):
     dev = p.get_device_info_by_index(i)
     print((i,dev['name'],dev['maxInputChannels']))
 
-
-import pyaudio
-import wave
-import os
 
 
 class RecAUD:
