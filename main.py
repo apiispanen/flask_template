37dcfa978028c9c73e1c6fdc12d7c771e1041af4
Andrew Piispanen <apiispanen1@babson.edu>
2023-02-21 22:31:39 -0500

    now I'm lost

--- main.py
+++ main.py
@@ -90,12 +90,10 @@ def save_audio():
 
 
 
-
-
-if __name__ == "__main__":
-    app.secret_key = 'mysecretkey'
-    # app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+# if __name__ == "__main__":
+app.secret_key = 'mysecretkey'
+# app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
 
 # app.run(debug=True, port=int(os.getenv("PORT", default=5000)))
 
